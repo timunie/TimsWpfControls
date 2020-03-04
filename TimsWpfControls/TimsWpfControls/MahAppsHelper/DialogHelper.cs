e3ed0bfc5f7236872e74d9a0104570b2991a4b43
Tim <tim.uniewski@gmail.com>
2020-03-04 20:32:00 +0100

    RelayCommand

---
+++ TimsWpfControls/TimsWpfControls/MahAppsHelper/DialogHelper.cs
@@ -0,0 +1,27 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TimsWpfControls
+{
+    public static class DialogHelper
+    {
+        public static async Task ShowErrorMessage(Exception e, string Header = "Error")
+        {
+            MetroWindow window = Application.Current.Windows.OfType<MetroWindow>().FirstOrDefault(x => x.IsActive);
+            if (window is null)
+            {
+                MessageBox.Show(e.Message, Header);
+            }
+            else
+            {
+                await window.ShowMessageAsync(Header, e.Message, MessageDialogStyle.Affirmative);
+            }
+        }
+    }
+}
---
+++ TimsWpfControls/TimsWpfControls/Model/RelayCommand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace TimsWpfControls.Model
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+
+        private Predicate<object> canExecute;
+
+        private event EventHandler CanExecuteChangedInternal;
+
+        public RelayCommand(Action<object> execute) : this(execute, DefaultCanExecute)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                this.CanExecuteChangedInternal += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                this.CanExecuteChangedInternal -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute != null && this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            EventHandler handler = this.CanExecuteChangedInternal;
+            if (handler != null)
+            {
+                handler.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void Destroy()
+        {
+            this.canExecute = _ => false;
+            this.execute = _ => { return; };
+        }
+
+        private static bool DefaultCanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
