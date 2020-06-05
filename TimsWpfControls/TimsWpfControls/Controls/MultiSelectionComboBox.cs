c36192b8171a7580ae242a02c379403dd2bd6a7b
Tim <tim.uniewski@gmail.com>
2020-06-05 12:28:47 +0200

    Initial code

---
+++ TimsWpfControls/TimsWpfControls/Controls/MultiSelectionComboBox.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TimsWpfControls
+{
+    public class MultiSelectionComboBox : ComboBox
+    {
+        static MultiSelectionComboBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MultiSelectionComboBox), new FrameworkPropertyMetadata(typeof(MultiSelectionComboBox)));
+        }
+
+        #region SelectionMode
+
+
+
+        public SelectionMode SelectionMode
+        {
+            get { return (SelectionMode)GetValue(SelectionModeProperty); }
+            set { SetValue(SelectionModeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectionMode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectionModeProperty =
+            DependencyProperty.Register("SelectionMode", typeof(SelectionMode), typeof(MultiSelectionComboBox), new FrameworkPropertyMetadata(SelectionMode.Single, new PropertyChangedCallback(OnSelectionModeChanged)),
+                        new ValidateValueCallback(IsValidSelectionMode));
+
+        private static void OnSelectionModeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is MultiSelectionComboBox multiSelectionComboBox)
+            {
+                multiSelectionComboBox.ValidateSelectionMode(multiSelectionComboBox.SelectionMode);
+            }
+        }
+
+        private void ValidateSelectionMode(SelectionMode selectionMode)
+        {
+            // throw new NotImplementedException();
+        }
+
+        private static bool IsValidSelectionMode(object o)
+        {
+            SelectionMode value = (SelectionMode)o;
+            return value == SelectionMode.Single
+                || value == SelectionMode.Multiple
+                || value == SelectionMode.Extended;
+        }
+
+
+        #endregion
+    }
+}
