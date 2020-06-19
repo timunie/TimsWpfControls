e8b51643df6bca2505033efb2e8473859bfe1616
Tim <tim.uniewski@gmail.com>
2020-06-19 12:45:06 +0200

    Improve Popup

--- TimsWpfControls/TimsWpfControls/Controls/MultiSelectionComboBox.cs
+++ TimsWpfControls/TimsWpfControls/Controls/MultiSelectionComboBox.cs
@@ -1,25 +1,19 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Markup;
 
 namespace TimsWpfControls
 {
     [TemplatePart (Name = nameof(PART_EditableTextBox), Type = typeof(TextBox))]
     [TemplatePart (Name = nameof(PART_Popup), Type = typeof(Popup))]
+    [TemplatePart (Name = nameof(PART_PopupItemsPresenter), Type = typeof(ItemsPresenter))]
     public class MultiSelectionComboBox : ListBox
     {
         private TextBox PART_EditableTextBox;
         private Popup PART_Popup;
+        private ItemsPresenter PART_PopupItemsPresenter;
 
         static MultiSelectionComboBox()
         {
@@ -34,7 +28,7 @@ namespace TimsWpfControls
         public static readonly DependencyProperty IsEditableProperty = DependencyProperty.Register("IsEditable", typeof(bool), typeof(MultiSelectionComboBox), new PropertyMetadata(false));
 
         // Using a DependencyProperty as the backing store for IsDropDownOpen.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty IsDropDownOpenProperty = DependencyProperty.Register("IsDropDownOpen", typeof(bool), typeof(MultiSelectionComboBox), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        public static readonly DependencyProperty IsDropDownOpenProperty = DependencyProperty.Register("IsDropDownOpen", typeof(bool), typeof(MultiSelectionComboBox), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnIsDropDownOpenChanged));
 
         // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TextProperty = 
@@ -65,6 +59,26 @@ namespace TimsWpfControls
             set { SetValue(IsDropDownOpenProperty, value); }
         }
 
+        private static void OnIsDropDownOpenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is MultiSelectionComboBox multiSelectionComboBox)
+            {
+                if ((bool)e.NewValue)
+                {
+                    multiSelectionComboBox.Dispatcher.BeginInvoke(
+                        new Action(() => 
+                        {
+                            var item = multiSelectionComboBox.SelectedItem ?? (multiSelectionComboBox.HasItems ? multiSelectionComboBox.Items[0] : null);
+                            if (item != null)
+                            {
+                                var listBoxItem = multiSelectionComboBox.ItemContainerGenerator.ContainerFromItem(item) as FrameworkElement;
+                                listBoxItem.Focus();
+                            }
+                        }), System.Windows.Threading.DispatcherPriority.Send);
+                }
+            }
+            
+        }
 
         public string Text
         {
@@ -111,11 +125,21 @@ namespace TimsWpfControls
         {
             base.OnApplyTemplate();
 
-            if (PART_EditableTextBox == null)
+            PART_EditableTextBox = GetTemplateChild(nameof(PART_EditableTextBox)) as TextBox;
+            PART_EditableTextBox.TextChanged += PART_EditableTextBox_TextChanged;
+            UpdateEditableText();
+
+            PART_Popup = GetTemplateChild(nameof(PART_Popup)) as Popup;
+            PART_PopupItemsPresenter = GetTemplateChild(nameof(PART_PopupItemsPresenter)) as ItemsPresenter;
+            PART_Popup.LostFocus += PART_Popup_LostFocus;
+
+        }
+
+        private void PART_Popup_LostFocus(object sender, RoutedEventArgs e)
+        {
+           if (!PART_Popup.IsKeyboardFocusWithin)
             {
-                PART_EditableTextBox = GetTemplateChild(nameof(PART_EditableTextBox)) as TextBox;
-                PART_EditableTextBox.TextChanged += PART_EditableTextBox_TextChanged; ;
-                UpdateEditableText();
+                SetCurrentValue(IsDropDownOpenProperty, false);
             }
         }
 
@@ -124,13 +148,15 @@ namespace TimsWpfControls
         {
             if (!isUpdatingText)
             {
-                Text = PART_EditableTextBox?.Text;
+                var text = PART_EditableTextBox?.Text;
 
-                SelectedItems.Clear();
-                if (!string.IsNullOrEmpty(Text))
+                if (text == string.Join(TextSeparator, (IEnumerable<object>)SelectedItems))
                 {
-                    var items = Text.Split(new string[] { TextSeparator }, StringSplitOptions.RemoveEmptyEntries);
-
+                    Text = null;
+                }
+                else
+                {
+                    Text = text;
                 }
             }
         }
@@ -142,6 +168,11 @@ namespace TimsWpfControls
             UpdateEditableText();
         }
 
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            SetCurrentValue(IsDropDownOpenProperty, false);
+        }
 
         #endregion
     }
