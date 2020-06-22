05da82bb27bd5734921023e80bc4a14d45abebb3
Tim <tim.uniewski@gmail.com>
2020-06-22 14:31:50 +0200

    Improve Demo view

--- TimsWpfControls/TimsWpfControls_Demo/Views/MultiselectionComboBox.xaml.cs
+++ TimsWpfControls/TimsWpfControls_Demo/Views/MultiselectionComboBox.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimsWpfControls;
 
 namespace TimsWpfControls_Demo.Views
 {
@@ -21,6 +23,13 @@ namespace TimsWpfControls_Demo.Views
         public MultiselectionComboBox()
         {
             InitializeComponent();
+
+            demoView.AddDemoProperty(MultiSelectionComboBox.TextProperty, multiSelectionComboBox);
+            demoView.AddDemoProperty(MultiSelectionComboBox.TextSeparatorProperty, multiSelectionComboBox);
+            demoView.AddDemoProperty(TextBoxHelper.ClearTextButtonProperty, multiSelectionComboBox);
+            demoView.AddDemoProperty(MultiSelectionComboBox.SelectionModeProperty, multiSelectionComboBox);
+            demoView.AddDemoProperty(MultiSelectionComboBox.IsEditableProperty, multiSelectionComboBox);
+            demoView.AddDemoProperty(MultiSelectionComboBox.IsReadOnlyProperty, multiSelectionComboBox);
         }
     }
 }
