c8e457b979e8b11a155ed1303155e5fe30eae21f
Tim <tim.uniewski@gmail.com>
2020-10-02 19:38:55 +0200

    Cancel the running DispatcherOperation before starting a new one

--- TimsWpfControls/TimsWpfControls/Controls/ColorPicker/ColorEyeDropper.cs
+++ TimsWpfControls/TimsWpfControls/Controls/ColorPicker/ColorEyeDropper.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -9,8 +10,19 @@ using System.Windows.Threading;
 
 namespace TimsWpfControls
 {
+    [TemplatePart(Name = nameof(PART_PreviewToolTip), Type = typeof(ToolTip))]
+    [TemplatePart(Name = nameof(PART_PreviewImage), Type = typeof(Image))]
     public class ColorEyeDropper : ContentControl
     {
+        static ColorEyeDropper()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorEyeDropper), new FrameworkPropertyMetadata(typeof(ColorEyeDropper)));
+        }
+
+        private ToolTip PART_PreviewToolTip;
+        private Image PART_PreviewImage;
+        private DispatcherOperation currentTask;
+
         // Depency Properties
         public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register("SelectedColor", typeof(Color), typeof(ColorEyeDropper), new FrameworkPropertyMetadata(Colors.Black, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
         public static readonly DependencyProperty PreviewBrushProperty = DependencyProperty.Register("PreviewBrush", typeof(Brush), typeof(ColorEyeDropper), new PropertyMetadata(Brushes.Transparent));
@@ -20,14 +32,13 @@ namespace TimsWpfControls
 
         public static readonly DependencyProperty EyeDropperCursorProperty = DependencyProperty.Register("EyeDropperCursor", typeof(Cursor), typeof(ColorEyeDropper), new PropertyMetadata(null));
 
-
         public Color SelectedColor
         {
             get { return (Color)this.GetValue(SelectedColorProperty); }
             set { this.SetValue(SelectedColorProperty, value); }
         }
 
-        public Brush PreviewColor
+        public Brush PreviewBrush
         {
             get { return (Brush)GetValue(PreviewBrushProperty); }
         }
@@ -55,11 +66,63 @@ namespace TimsWpfControls
             set { SetValue(EyeDropperCursorProperty, value); }
         }
 
-
-        private void ColorEyeDropper_PreviewMouseUp(object sender, MouseEventArgs e)
+        private void SetPreview()
         {
-            this.ReleaseMouseCapture();
-            this.MouseMove -= this.ColorEyeDropper_PreviewMouseMove;
+            if (currentTask?.Status == DispatcherOperationStatus.Executing || currentTask?.Status == DispatcherOperationStatus.Pending)
+            {
+                currentTask.Abort();
+            }
+
+            var action = new Action(() =>
+            {
+                var mousePos = EyeDropperHelper.GetCursorPosition();
+                var previewImage = EyeDropperHelper.CaptureRegion(new Int32Rect(mousePos.X - PreviewImageOuterPixelCount, mousePos.Y - PreviewImageOuterPixelCount, 2 * PreviewImageOuterPixelCount + 1, 2 * PreviewImageOuterPixelCount + 1));
+                var previewBrush = new SolidColorBrush(EyeDropperHelper.GetPixelColor(mousePos));
+                previewBrush.Freeze();
+
+                PART_PreviewImage.Source = previewImage;
+                SetCurrentValue(PreviewBrushProperty, previewBrush);
+            });
+
+            currentTask = Dispatcher.BeginInvoke(DispatcherPriority.Background, action);
+        }
+
+        #region Overrides
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            this.PART_PreviewToolTip = GetTemplateChild(nameof(PART_PreviewToolTip)) as ToolTip;
+            this.PART_PreviewImage = GetTemplateChild(nameof(PART_PreviewImage)) as Image;
+        }
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+
+            Mouse.Capture(this);
+
+            if (!(PART_PreviewToolTip is null))
+            {
+                PART_PreviewToolTip.Visibility = Visibility.Visible;
+                PART_PreviewToolTip.IsOpen = true;
+            }
+
+            this.Cursor = this.EyeDropperCursor;
+
+            SetPreview();
+        }
+
+        protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonUp(e);
+
+            Mouse.Capture(null);
+
+            if (!(PART_PreviewToolTip is null))
+            {
+                PART_PreviewToolTip.IsOpen = false;
+                PART_PreviewToolTip.Visibility = Visibility.Collapsed;
+            }
 
             this.Cursor = Cursors.Arrow;
 
@@ -68,56 +131,20 @@ namespace TimsWpfControls
                 var mousePos = EyeDropperHelper.GetCursorPosition();
                 this.SelectedColor = EyeDropperHelper.GetPixelColor(mousePos);
             }
+        }
 
-            if (this.ToolTip is ToolTip toolTip)
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (e.LeftButton == MouseButtonState.Pressed)
             {
-                toolTip.IsOpen = false;
+                PART_PreviewToolTip.Placement = PlacementMode.Relative;
+                PART_PreviewToolTip.HorizontalOffset = e.GetPosition(PART_PreviewToolTip.PlacementTarget).X + 16;
+                PART_PreviewToolTip.VerticalOffset = e.GetPosition(PART_PreviewToolTip.PlacementTarget).Y + 16;
+
+                SetPreview();
             }
-        }
-
-        private void ColorEyeDropper_PreviewMouseDown(object sender, MouseEventArgs e)
-        {
-            this.PreviewMouseMove += this.ColorEyeDropper_PreviewMouseMove;
-            this.Cursor = Cursors.Cross;
-            Mouse.Capture(this);
-
-            if (this.ToolTip is ToolTip toolTip)
-            {
-                toolTip.PlacementTarget = this;
-                toolTip.Placement = System.Windows.Controls.Primitives.PlacementMode.Mouse;
-                toolTip.IsOpen = true;
-            }
-
-            SetPreview();
-            
-        }
-
-        private void ColorEyeDropper_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            SetPreview();
-        }
-
-        private void SetPreview()
-        {
-            var action = new Action(() =>
-            {
-                var mousePos = EyeDropperHelper.GetCursorPosition();
-                var previewImage = EyeDropperHelper.CaptureRegion(new Int32Rect(mousePos.X - PreviewImageOuterPixelCount, mousePos.Y - PreviewImageOuterPixelCount, 2 * PreviewImageOuterPixelCount + 1, 2 * PreviewImageOuterPixelCount + 1));
-                var previewColor = EyeDropperHelper.GetPixelColor(mousePos);
-
-                SetCurrentValue(PreviewImageSourceProperty, previewImage);
-                SetCurrentValue(PreviewBrushProperty, new SolidColorBrush(previewColor));
-            });
-            Dispatcher.BeginInvoke(DispatcherPriority.Background, action);
-        }
-
-        #region Overrides
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-
-            this.PreviewMouseDown += ColorEyeDropper_PreviewMouseDown;
-            this.PreviewMouseUp += ColorEyeDropper_PreviewMouseUp;
         }
         #endregion
 
