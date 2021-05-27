a6df0b89dd948c96745c934f17cbca78e2d5f81f
Tim <tim.uniewski@gmail.com>
2021-05-27 16:23:21 +0200

    Implementing own ValidationAttributes
    - LowerThanAttribute
    - FileOnNetworkAttribute
    - CollectionNotEmptyAttribute

---
+++ TimsWpfControls/TimsWpfControls/Validation/CollectionNotEmptyAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimsWpfControls.Validation
+{
+    public class CollectionNotEmptyAttribute : ValidationAttribute
+    {
+        public CollectionNotEmptyAttribute() : base(() => Lang.ValidationMessages.CollectionMayNotBeEmpty)
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is null)
+            {
+                return true;
+            }
+            if (value is IEnumerable collection)
+            {
+                return collection.GetEnumerator().MoveNext();
+            }
+            else
+            {
+                throw new ArgumentException(null, nameof(value));
+            }
+        }
+    }
+}
---
+++ TimsWpfControls/TimsWpfControls/Validation/GreaterThanAttribute.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimsWpfControls.Validation
+{
+    public class GreaterThanAttribute : ValidationAttribute
+    {
+        public object CompareTo { get; private set; }
+
+        public Type DataType { get; private set; }
+
+        public GreaterThanAttribute(double compareTo) : this()
+        {
+            CompareTo = compareTo;
+            DataType = typeof(double);
+        }
+
+        public GreaterThanAttribute(int compareTo) : this()
+        {
+            CompareTo = compareTo;
+            DataType = typeof(int);
+        }
+
+        private GreaterThanAttribute() : base (() => Lang.ValidationMessages.PropertyMustBeGreaterThanMessage)
+        {
+            // Empty
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is null)
+            {
+                return true;
+            }
+            else if (DataType == typeof(double))
+            {
+                return Convert.ToDouble(value) > (double)CompareTo;
+            }
+            else if (DataType == typeof(int))
+            {
+                return Convert.ToInt32(value) > (int)CompareTo;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, this.CompareTo);
+        }
+    }
+}
