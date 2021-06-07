e2851b3184fcac9a3842257fdff7abc9093ef342
Tim <tim.uniewski@gmail.com>
2021-06-07 11:46:30 +0200

    Add xmlns-prefix
    Add DataGridIsNewRowToVisibilityConverter

--- TimsWpfControls/TimsWpfControls/AssemblyInfo.cs
+++ TimsWpfControls/TimsWpfControls/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -8,3 +9,8 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+
+[assembly: XmlnsPrefix(@"https://github.com/timunie/TimsWpfControls", "timsWpf")]
+[assembly: XmlnsDefinition(@"https://github.com/timunie/TimsWpfControls", "TimsWpfControls")]
+[assembly: XmlnsDefinition(@"https://github.com/timunie/TimsWpfControls", "TimsWpfControls.Converter")]
