53c5b5cc5857c7594e859660dc884322204c60dc
Tim <tim.uniewski@gmail.com>
2021-08-16 09:58:45 +0200

    Implement NullImageConverter

---
+++ TimsWpfControls/TimsWpfControls/Converter/NullImageConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace TimsWpfControls.Converter
+{
+    public class NullImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is null ? DependencyProperty.UnsetValue : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
