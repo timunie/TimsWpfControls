50f019c2fc01dcd2ef7a10810c82e28ecad42fe3
Tim <tim.uniewski@gmail.com>
2021-08-16 15:51:40 +0200

    DataGridIsNewRowToVisibilityConverter
    - Hides an element in new datagrid row

---
+++ TimsWpfControls/TimsWpfControls/Converter/DataGridIsNewRowToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TimsWpfControls
+{
+    public class DataGridIsNewRowToVisibilityConverter : IValueConverter
+    {
+        private static DataGridIsNewRowToVisibilityConverter _Instance;
+        public static DataGridIsNewRowToVisibilityConverter Instance => _Instance ??= new();
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == CollectionView.NewItemPlaceholder ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
